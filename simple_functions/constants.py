f3c3d5158ea837c9945e912c56a7c7df1647b907
acse-dde6579c <wei.zhong22@imperial.ac.uk>
2022-10-18 11:49:46 +0100

    new 2 func

--- simple_functions/constants.py
+++ simple_functions/constants.py
@@ -13,16 +13,4 @@ def pi(terms=1):
 def rsum(n):
     t = factorial(4*n)*(1103+26390*n)/(factorial(n)**4*396**(4*n))
     return t + rsum(n-1) if n else t
-Then, lets add a new test file called test_constants.py. This should look something like the following:
-import numpy as np
 
-from simple_functions import pi
-
-
-class TestPi(object):
-    '''Class to test our constants are computed correctly'''
-
-    def test_pi(self):
-        '''Test computation of pi'''
-        my_pi = pi(2)
-        assert np.isclose(my_pi, np.pi, atol=1e-12)
